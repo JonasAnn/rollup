c132f1b21c1a9662e8dd5a2057ac0c74dba2af93
Ifeoma Jonah <ifyajoke@gmail.com>
2020-09-18 02:14:54 -0700

    changed time

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 26, 2020 12:00:00").getTime();
+var countDownDate = new Date("Oct 11, 2020 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
